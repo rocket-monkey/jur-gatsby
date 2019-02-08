bd063f21b93486bbb6fbae52d7cf69079e7d0aab
Remo Vetere <Remo.Vetere@digitecgalaxus.ch>
2019-02-08 17:06:00 +0100

    improve

--- src/components/BgVideo/index.js
+++ src/components/BgVideo/index.js
@@ -1,50 +1,22 @@
 import React, { PureComponent } from 'react'
 import classNames from 'class-names'
+import { Parallax } from 'react-scroll-parallax'
 
-import JurLogo from '../icons/JurLogo'
 import JurLogoFont from '../icons/JurLogoFont'
 import styles from './styles.module.scss'
 
 export default class BgVideo extends PureComponent {
   ref = React.createRef()
   refVideo = React.createRef()
+
   state = {
     showLogo: false,
-    top: '50%',
-    transform: 'translate(-50%, -50%)',
-  }
-
-  parallaxImg = () => {
-    var speed = 0.7
-    var imgY = this.ref.current.getBoundingClientRect().top
-    var winY = window.scrollY
-    var winH = window.innerHeight
-    var parentH = this.ref.current.offsetHeight
-
-    // The next pixel to show on screen
-    var winBottom = winY + winH
-
-    // If block is shown on screen
-    if (winBottom > imgY && winY < imgY + parentH) {
-      // Number of pixels shown after block appear
-      var imgBottom = (winBottom - imgY) * speed
-      // Max number of pixels until block disappear
-      var imgTop = winH + parentH
-      // Porcentage between start showing until disappearing
-      var imgPercent = (imgBottom / imgTop) * 100 + (50 - speed * 50)
-    }
-    this.setState({
-      top: imgPercent + '%',
-      transform: 'translate(-50%, -' + imgPercent + '%)',
-    })
   }
 
   componentDidMount() {
-    this.parallaxImg()
     setTimeout(() => {
       this.setState({ showLogo: true })
     }, 800)
-    window.addEventListener('scroll', this.parallaxImg, { passive: true })
 
     // fuck you cock-juggling thunder-cunts @apple, FUCK YOU!
     const ua = navigator.userAgent.toLowerCase()
@@ -56,36 +28,38 @@ export default class BgVideo extends PureComponent {
     }
   }
 
-  componentWillUnmount = () => {
-    window.removeEventListener('scroll', this.parallaxImg, { passive: true })
-  }
-
   render() {
-    const styleObj = { top: this.state.top, transform: this.state.transform }
     return (
       <>
-        <div
-          className={classNames(styles.wrapper, {
-            [styles.showLogo]: this.state.showLogo,
-          })}
-          ref={this.ref}
+        <Parallax
+          className="custom-class"
+          offsetYMax={20}
+          offsetYMin={-20}
+          slowerScrollRate
+          tag="div"
         >
-          <video
-            id="idForSafari"
-            playsInline
-            autoPlay
-            muted
-            loop
-            ref={this.refVideo}
-            style={styleObj}
+          <div
+            className={classNames(styles.wrapper, {
+              [styles.showLogo]: this.state.showLogo,
+            })}
+            ref={this.ref}
           >
-            <source type="video/webm" src="/img/bgvideo.webm" />
-            <source type="video/ogg" src="/img/bgvideo.ogg" />
-            <source type="video/mp4" src="/img/bgvideo.mp4" />
-          </video>
+            <video
+              id="idForSafari"
+              playsInline
+              autoPlay
+              muted
+              loop
+              ref={this.refVideo}
+            >
+              <source type="video/webm" src="/img/bgvideo.webm" />
+              <source type="video/ogg" src="/img/bgvideo.ogg" />
+              <source type="video/mp4" src="/img/bgvideo.mp4" />
+            </video>
 
-          <JurLogoFont style={styleObj} />
-        </div>
+            <JurLogoFont />
+          </div>
+        </Parallax>
 
         {this.props.children}
       </>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import classNames from 'class-names'
-import { CSSTransition } from 'react-transition-group'
-import BodyClassName from 'react-body-classname'
-import JurLogo from '../icons/JurLogo'
 import BgVideo from '../../components/BgVideo'
 import styles from './styles.module.scss'
 
@@ -12,130 +9,42 @@ const Navbar = class extends React.Component {
     burgerActive: false,
   }
 
-  componentDidMount() {
-    if (window.innerWidth >= 1088) {
-      this.setState({ burgerActive: true })
-    }
-  }
-
   render() {
     const { burgerActive } = this.state
-
-    const menuJsx = (
-      <div className={styles.menu}>
+    return (
+      <>
+        <BgVideo />
         <div
-          className={classNames(
-            'navbar-start',
-            'has-text-centered',
-            styles.links
-          )}
-          onClick={this.handleLinkClick}
+          className={classNames('navbar-burger', 'burger', {
+            'is-active': burgerActive,
+          })}
+          onClick={this.handleBurgerClick}
         >
-          <Link className="navbar-item" to="/spirit">
-            Spirit
-          </Link>
-          <Link className="navbar-item" to="/crew">
-            Crew
-          </Link>
-          <Link className="navbar-item" to="/events">
-            Events
-          </Link>
-          <Link className="navbar-item" to="/store">
-            Store
-          </Link>
-          <Link className="navbar-item" to="/ware">
-            Ware
-          </Link>
+          <span />
+          <span />
+          <span />
+        </div>
+
+        <div
+          className={classNames(styles.navbar, { [styles.open]: burgerActive })}
+        >
+          <Link to="/spirit">Spirit</Link>
+          <Link to="/crew">Crew</Link>
+          <Link to="/events">Events</Link>
+          <Link to="/store">Store</Link>
+          <Link to="/ware">Ware</Link>
           {/*
-            <Link className="navbar-item" to="/contact/examples">
+            <Link to="/contact/examples">
               Form Examples
             </Link>
           */}
         </div>
-        <div className="navbar-end has-text-centered" />
-      </div>
-    )
-
-    return (
-      <>
-        <BgVideo />
-        <nav
-          className={styles.navbar}
-          role="navigation"
-          aria-label="main-navigation"
-        >
-          <div className="container">
-            <div className="navbar-brand">
-              <BodyClassName
-                className={classNames({
-                  [styles.bodyNavbarActive]: burgerActive,
-                })}
-              >
-                <Link
-                  to="/"
-                  className={classNames('navbar-item', styles.logo, {
-                    [styles.logoActive]: burgerActive,
-                  })}
-                  onClick={this.handleLinkClick}
-                >
-                  <JurLogo />
-                </Link>
-              </BodyClassName>
-
-              {/* Hamburger menu */}
-              <div
-                className={classNames(
-                  'navbar-burger',
-                  'burger',
-                  styles.burger,
-                  {
-                    'is-active': burgerActive,
-                  }
-                )}
-                onClick={this.handleBurgerClick}
-              >
-                <span />
-                <span />
-                <span />
-              </div>
-              <CSSTransition
-                in={burgerActive}
-                classNames={{
-                  enter: styles.enter,
-                  enterActive: styles.enterActive,
-                  appear: styles.enter,
-                  appearActive: styles.enterActive,
-                  exit: styles.exit,
-                  exitActive: styles.exitActive,
-                  exitDone: styles.exitDone,
-                }}
-                timeout={150}
-                unmountOnExit
-                mountOnEnter
-              >
-                {menuJsx}
-              </CSSTransition>
-            </div>
-          </div>
-        </nav>
       </>
     )
   }
 
   handleBurgerClick = () => {
-    if (window.innerWidth >= 1088) {
-      return
-    }
-
     this.setState({ burgerActive: !this.state.burgerActive })
-  }
-
-  handleLinkClick = () => {
-    if (window.innerWidth >= 1088) {
-      return
-    }
-
-    this.setState({ burgerActive: false })
   }
 }
 
