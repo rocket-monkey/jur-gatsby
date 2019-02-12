a51bef77aedf9318e59a23efb72a74730372e571
Remo Vetere <remo.vetere@gmail.com>
2019-02-12 21:34:56 +0100

    help

--- src/cms/preview-templates/EventPostPreview.js
+++ src/cms/preview-templates/EventPostPreview.js
@@ -2,16 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { EventPageTemplate } from '../../templates/event-post'
 
-const EventPostPreview = ({ entry, widgetFor }) => (
-  <EventPageTemplate
-    isPreview
-    content={widgetFor('body')}
-    title={entry.getIn(['data', 'title'])}
-    image={entry.getIn(['data', 'image'])}
-    timeTable={entry.getIn(['data', 'timeTable'])}
-    image={entry.getIn(['data', 'location'])}
-  />
-)
+const EventPostPreview = ({ entry, widgetFor }) => {
+  console.log(entry.getIn(['data', 'image']))
+  console.log(entry)
+  return (
+    <EventPageTemplate
+      isPreview
+      content={widgetFor('body')}
+      title={entry.getIn(['data', 'title'])}
+      image={entry.getIn(['data', 'image'])}
+      timeTable={entry.getIn(['data', 'timeTable'])}
+      image={entry.getIn(['data', 'location'])}
+    />
+  )
+}
 
 EventPostPreview.propTypes = {
   entry: PropTypes.shape({
