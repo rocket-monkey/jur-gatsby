e8e167ac15b523218b80ddf361f416adb5794aa3
Remo Vetere <Remo.Vetere@digitecgalaxus.ch>
2019-04-23 13:28:05 +0200

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,13 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: 'UA-138890304-1',
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
