13cd8db899e95144d384b3d6d44756a74bbe39e8
Remo Vetere <remo.vetere@digitecgalaxus.ch>
2019-08-24 09:50:58 +0200

    Link artists

--- src/components/TimeTable/index.js
+++ src/components/TimeTable/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Table from '../Table'
-// import styles from './styles.module.scss'
 
 const TimeTable = ({ timeTable, artists, crew }) => (
   <Table>
@@ -10,12 +9,32 @@ const TimeTable = ({ timeTable, artists, crew }) => (
         let entryAct = entry.act
 
         const foundArtist =
-          artists && artists.find(a => a.node.frontmatter.title === entry.act)
+          artists &&
+          artists.find(
+            a =>
+              a.node.frontmatter.title.toLowerCase() ===
+              entry.act
+                .replace(/\(([A-Z])\w+\)/gi, '')
+                .trim()
+                .toLowerCase()
+          )
         const foundCrew =
-          crew && crew.find(a => a.node.frontmatter.title === entry.act)
-        console.log({ foundArtist, foundCrew })
+          crew &&
+          crew.find(
+            a =>
+              a.node.frontmatter.title.toLowerCase() === entry.act.toLowerCase()
+          )
 
-        if (foundCrew) {
+        if (foundArtist) {
+          entryAct = (
+            <a
+              href={`https://${foundArtist.node.frontmatter.page}`}
+              target="_blank"
+            >
+              {entry.act}
+            </a>
+          )
+        } else if (foundCrew) {
           entryAct = <Link to={foundCrew.node.fields.slug}>{entry.act}</Link>
         }
 
